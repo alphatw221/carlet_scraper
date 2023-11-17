717e0b013a8e95467ff5772c90799dace8df9feb
asdf50003206 <asdf500032061@gmail.com>
2023-11-17 20:52:51 +0800

    update

--- carlet_api.py
+++ carlet_api.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 
 from fastapi import FastAPI, Depends
 # from pydantic import BaseModel
@@ -42,28 +42,41 @@ def get_auto_data_db():
     with db.auto_data.Session() as session:
         yield session
 
+def get_tire_rack_db():
+    with db.tire_rack.Session() as session:
+        yield session
 
 class CarletVehicleOut(BaseModel):
-    # id: int|None
-    # name_1:str|None
-    # name: str|None
+    id: int|None
 
-    # year: int|None
-    # name_variant: str|None
+    make: str|None
+
+    name: str|None
+
+    year: int|None
+    name_variant: str|None
     pass
 
 
-class AutoDataVehicleVehicleOut(BaseModel):
-    
+class Property(BaseModel):
+    name:str|None
+    value:str|None
+class AutoDataVehicleOut(BaseModel):
+    id:int
     make: str
     model: str
-
     start_of_perduction_year:int|None
     end_of_perduction_year:int|None
-
     sub_model: str
+    # properties: List[Property]
 
-
+class TireRackVehicleOut(BaseModel):
+    id:int
+    make: str
+    model: str
+    # start_of_perduction_year:int|None
+    # end_of_perduction_year:int|None
+    sub_model: str
 
 # @app.get('/users', response_model=Page[db.local_carlet.models.Vehicle])  # use Page[UserOut] as response model
 # async def get_users():
@@ -82,16 +95,15 @@ def get_carlet_vehicles(_db:db.local_carlet.Session = Depends(get_carlet_db),
     vehicle_model = aliased(db.local_carlet.models.VehicleModel, name='vehicle_model')
     vehicle_make = aliased(db.local_carlet.models.VehicleMake, name='vehicle_make')
 
-    # 连接两个表格
-    # query = session.query(table1.name.label('table1_name'), 
-    #                     table2.name.label('table2_name')).\
-    #         join(table2_alias, table1.name == table2_alias.name)
+    query = select(
+        vehicle_model.id, 
+        vehicle_make.name.label('make'),
+        vehicle_model.year, 
+        vehicle_model.name, 
+        vehicle_model.name_variant, 
+        )\
+        .join(vehicle_make)
 
-    # 执行查询
-
-    query = select(vehicle_model, vehicle_make)\
-        .join(vehicle_make)\
-        # .where(db.local_carlet.models.VehicleModel.make_id==db.local_carlet.models.VehicleMake.id)
 
     
     if make:
@@ -104,13 +116,14 @@ def get_carlet_vehicles(_db:db.local_carlet.Session = Depends(get_carlet_db),
         query = query.filter(vehicle_model.name.contains(name))
     if keyword:
         query = query.filter(or_(
-                                    vehicle_make.name.contains(keyword), 
-                                    vehicle_model.name.contains(keyword),
-                                    vehicle_model.variant.contains(keyword),
-                                    vehicle_model.name_variant.contains(keyword),
-                                    vehicle_model.engine.contains(keyword),
-                                    vehicle_model.chassis.contains(keyword),
-                                 ))
+            vehicle_make.name.contains(keyword), 
+            vehicle_model.name.contains(keyword),
+            vehicle_model.variant.contains(keyword),
+            vehicle_model.name_variant.contains(keyword),
+            vehicle_model.engine.contains(keyword),
+            vehicle_model.chassis.contains(keyword),
+        ))
+
     if order_by:
         order_by_list = order_by.split((','))
         for _order_by in order_by_list:
@@ -131,30 +144,127 @@ def get_carlet_vehicles(_db:db.local_carlet.Session = Depends(get_carlet_db),
                 else:
                     query = query.order_by(getattr(db.local_carlet.models.VehicleMake, _order_by).desc())
 
-
-    print(query)
-
-    
     return paginate(_db, query)
 
 
 
 
 
-@app.get('/auto_data/vehicles', response_model=Page[AutoDataVehicleVehicleOut])
+@app.get('/auto_data/vehicles', response_model=Page[AutoDataVehicleOut])
 def get_carlet_vehicles(_db:db.auto_data.Session = Depends(get_auto_data_db), 
                         make:str|None=None, 
                         model:str|None=None, 
                         sub_model:str|None=None, 
                         start_of_perduction_year:int|None=None,
                         end_of_perduction_year:int|None=None,
-                        keyword:str|None=None):    
-
-    return paginate(_db, select(db.auto_data.car.Car).order_by(db.auto_data.car.Car.created_at))
-
-
+                        keyword:str|None=None,
+                        order_by:str|None=None):    
+ 
 
 
+    car = aliased(db.auto_data.car.Car, name='car')
+    # property = aliased(db.auto_data.car.Property, name='property')
+
+    query = select(
+        car.id, 
+        car.make,
+        car.model,
+        car.sub_model,
+        car.start_of_perduction_year,
+        car.end_of_perduction_year,
+        )
+    
+    if make:
+        query = query.filter(car.make.ilike(f'%{make}%'))
+    if start_of_perduction_year:
+        query = query.filter(car.start_of_perduction_year>=start_of_perduction_year)
+    if end_of_perduction_year:
+        query = query.filter(car.end_of_perduction_year<end_of_perduction_year)
+    if model:
+        query = query.filter(car.model.ilike(f'%{model}%'))
+    if sub_model:
+        query = query.filter(car.sub_model.ilike(f'%{sub_model}%'))
+    if keyword:
+        query = query.filter(or_(
+            car.make.ilike(f'%{keyword}%'), 
+            car.model.ilike(f'%{keyword}%'),
+            car.sub_model.ilike(f'%{keyword}%'),
+        ))
+
+    if order_by:
+        order_by_list = order_by.split((','))
+        for _order_by in order_by_list:
+            asc = True
+            if _order_by[0] == '-':
+                asc = False
+                _order_by = _order_by[1:]
+                
+            if hasattr(db.auto_data.car.Car, _order_by) :
+                if asc:
+                    query = query.order_by(getattr(db.auto_data.car.Car, _order_by).asc())
+                else:
+                    query = query.order_by(getattr(db.auto_data.car.Car, _order_by).desc())
+
+    return paginate(_db, query)
+
+
+@app.get('/tire_rack/vehicles', response_model=Page[TireRackVehicleOut])
+def get_tire_rack_vehicles(_db:db.tire_rack.Session = Depends(get_tire_rack_db), 
+                        make:str|None=None, 
+                        model:str|None=None, 
+                        sub_model:str|None=None, 
+                        start_of_perduction_year:int|None=None,
+                        end_of_perduction_year:int|None=None,
+                        keyword:str|None=None,
+                        order_by:str|None=None):    
+ 
+
+
+    car = aliased(db.tire_rack.car.Car, name='car')
+    # property = aliased(db.auto_data.car.Property, name='property')
+
+    query = select(
+        car.id, 
+        car.make,
+        car.model,
+        car.sub_model,
+        car.year
+        # car.start_of_perduction_year,
+        # car.end_of_perduction_year,
+        )
+    
+    if make:
+        query = query.filter(car.make.ilike(f'%{make}%'))
+    if start_of_perduction_year:
+        query = query.filter(car.year>=start_of_perduction_year)
+    if end_of_perduction_year:
+        query = query.filter(car.year<end_of_perduction_year)
+    if model:
+        query = query.filter(car.model.ilike(f'%{model}%'))
+    if sub_model:
+        query = query.filter(car.sub_model.ilike(f'%{sub_model}%'))
+    if keyword:
+        query = query.filter(or_(
+            car.make.ilike(f'%{keyword}%'), 
+            car.model.ilike(f'%{keyword}%'),
+            car.sub_model.ilike(f'%{keyword}%'),
+        ))
+
+    if order_by:
+        order_by_list = order_by.split((','))
+        for _order_by in order_by_list:
+            asc = True
+            if _order_by[0] == '-':
+                asc = False
+                _order_by = _order_by[1:]
+                
+            if hasattr(db.tire_rack.car.Car, _order_by) :
+                if asc:
+                    query = query.order_by(getattr(db.tire_rack.car.Car, _order_by).asc())
+                else:
+                    query = query.order_by(getattr(db.tire_rack.car.Car, _order_by).desc())
+
+    return paginate(_db, query)
 
 
 add_pagination(app)  # important! add pagination to your app
