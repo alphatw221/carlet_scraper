fb2d39ad942088faf767f9f29fb5215f646f14bb
lin <yihsuehlin@accoladeglobal.com>
2023-12-26 12:48:22 +0800

    update

--- routers/vehicle/__init__.py
+++ routers/vehicle/__init__.py
@@ -104,85 +104,100 @@ def get_carlet_vehicles(current_user: Annotated[lib.helper.auth_helper.User, Dep
     #     )\
     #     .join(vehicle_make)
     
-    vehicles = _db.query(db.local_carlet.models.VehicleModel).options(selectinload(db.local_carlet.models.VehicleModel.make))
+    vehicles = _db.query(db.local_carlet.models.VehicleModel).join(db.local_carlet.models.VehicleMake).options(selectinload(db.local_carlet.models.VehicleModel.make))
 
-    # if id and id.isnumeric():
-    #     query = query.filter(vehicle_model.id==int(id))
-    #     vehicle = vehicle.filter(db.local_carlet.models.VehicleModel==int(id))
-    # if auto_data_id and lib.utils.is_number(auto_data_id):
-    #     query = query.filter(vehicle_model.auto_data_id==int(auto_data_id))
-    # if tire_rack_id and lib.utils.is_number(tire_rack_id):
-    #     query = query.filter(vehicle_model.tire_rack_id==int(tire_rack_id))
-    # if yahoo_id and lib.utils.is_number(yahoo_id):
-    #     query = query.filter(vehicle_model.yahoo_id==int(yahoo_id))
-    # if make:
-    #     query = query.filter(vehicle_make.name.ilike(f'%{make}%'))
-    # if start_of_production_year and start_of_production_year.isnumeric():
-    #     query = query.filter(vehicle_model.year>=int(start_of_production_year))
-    # if end_of_production_year and end_of_production_year.isnumeric():
-    #     query = query.filter(vehicle_model.year<int(end_of_production_year))
-    # if model:
-    #     for sub_string in model.split(','):
-    #         if not sub_string:
-    #             continue
-    #         query = query.filter(vehicle_model.name.ilike(f'%{sub_string}%'))
-    # if exclude_model:
-    #     for sub_string in exclude_model.split(','):
-    #         if not sub_string:
-    #             continue
-    #         query = query.filter(not_(vehicle_model.name.ilike(f'%{sub_string}%')))
-    # if sub_model:
-    #     for sub_string in sub_model.split(','):
-    #         if not sub_string:
-    #             continue
-    #         query = query.filter(vehicle_model.name_variant.ilike(f'%{sub_string}%'))
-    # if exclude_sub_model:
-    #     print(exclude_sub_model)
-    #     for sub_string in exclude_sub_model.split(','):
-    #         if not sub_string:
-    #             continue
-    #         query = query.filter(not_(vehicle_model.name_variant.ilike(f'%{sub_string}%')))
-    # # if keyword:
-    # #     query = query.filter(or_(
-    # #         vehicle_make.name.label('make').ilike(f'%{keyword}%'), 
-    # #         vehicle_model.name.ilike(f'%{keyword}%'),
-    # #         vehicle_model.variant.ilike(f'%{keyword}%'),
-    # #         vehicle_model.name_variant.ilike(f'%{keyword}%'),
-    # #         vehicle_model.engine.ilike(f'%{keyword}%'),
-    # #         vehicle_model.chassis.ilike(f'%{keyword}%'),
-    # #     ))
-    # if engine:
-    #     query = query.filter(vehicle_model.engine.ilike(f'%{engine}%'))
-    # if chassis:
-    #     query = query.filter(vehicle_model.chassis.ilike(f'%{chassis}%'))
-    # if exclude_auto_data_done_mapping == 'true':
-    #     query = query.filter(vehicle_model.auto_data_id==None)
+    if id and id.isnumeric():
+        # query = query.filter(vehicle_model.id==int(id))
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.id==int(id))
+    if auto_data_id and lib.utils.is_number(auto_data_id):
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.auto_data_id==int(auto_data_id))
 
-    # if exclude_tire_rack_done_mapping == 'true':
-    #     query = query.filter(vehicle_model.tire_rack_id==None)
+        # query = query.filter(vehicle_model.auto_data_id==int(auto_data_id))
+    if tire_rack_id and lib.utils.is_number(tire_rack_id):
+        # query = query.filter(vehicle_model.tire_rack_id==int(tire_rack_id))
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.tire_rack_id==int(tire_rack_id))
+    if yahoo_id and lib.utils.is_number(yahoo_id):
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.yahoo_id==int(yahoo_id))
+        # query = query.filter(vehicle_model.yahoo_id==int(yahoo_id))
+    if make:
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleMake.name.ilike(f'%{make}%'))
+        # query = query.filter(vehicle_make.name.ilike(f'%{make}%'))
+    if start_of_production_year and start_of_production_year.isnumeric():
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.year>=int(start_of_production_year))
+        # query = query.filter(vehicle_model.year>=int(start_of_production_year))
+    if end_of_production_year and end_of_production_year.isnumeric():
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.year<int(end_of_production_year))
+        # query = query.filter(vehicle_model.year<int(end_of_production_year))
+    if model:
+        for sub_string in model.split(','):
+            if not sub_string:
+                continue
+            vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.name.ilike(f'%{sub_string}%'))
+            # query = query.filter(vehicle_model.name.ilike(f'%{sub_string}%'))
+    if exclude_model:
+        for sub_string in exclude_model.split(','):
+            if not sub_string:
+                continue
+            vehicles = vehicles.filter(not_(db.local_carlet.models.VehicleModel.name.ilike(f'%{sub_string}%')))
+            # query = query.filter(not_(vehicle_model.name.ilike(f'%{sub_string}%')))
+    if sub_model:
+        for sub_string in sub_model.split(','):
+            if not sub_string:
+                continue
+            vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.name_variant.ilike(f'%{sub_string}%'))
+            # query = query.filter(vehicle_model.name_variant.ilike(f'%{sub_string}%'))
+    if exclude_sub_model:
+        print(exclude_sub_model)
+        for sub_string in exclude_sub_model.split(','):
+            if not sub_string:
+                continue
+            vehicles = vehicles.filter(not_(db.local_carlet.models.VehicleModel.name_variant.ilike(f'%{sub_string}%')))
+            # query = query.filter(not_(vehicle_model.name_variant.ilike(f'%{sub_string}%')))
+    # if keyword:
+    #     query = query.filter(or_(
+    #         vehicle_make.name.label('make').ilike(f'%{keyword}%'), 
+    #         vehicle_model.name.ilike(f'%{keyword}%'),
+    #         vehicle_model.variant.ilike(f'%{keyword}%'),
+    #         vehicle_model.name_variant.ilike(f'%{keyword}%'),
+    #         vehicle_model.engine.ilike(f'%{keyword}%'),
+    #         vehicle_model.chassis.ilike(f'%{keyword}%'),
+    #     ))
+    if engine:
+        # query = query.filter(vehicle_model.engine.ilike(f'%{engine}%'))
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.engine.ilike(f'%{engine}%'))
+    if chassis:
+        # query = query.filter(vehicle_model.chassis.ilike(f'%{chassis}%'))
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.chassis.ilike(f'%{chassis}%'))
+    if exclude_auto_data_done_mapping == 'true':
+        # query = query.filter(vehicle_model.auto_data_id==None)
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.auto_data_id==None)
+    if exclude_tire_rack_done_mapping == 'true':
+        # query = query.filter(vehicle_model.tire_rack_id==None)
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.tire_rack_id==None)
+    if exclude_yahoo_done_mapping == 'true':
+        # query = query.filter(vehicle_model.yahoo_id==None)
+        vehicles = vehicles.filter(db.local_carlet.models.VehicleModel.yahoo_id==None)
+    if order_by:
+        order_by_list = order_by.split(',')
+        for _order_by in order_by_list:
+            asc = True
 
-    # if exclude_yahoo_done_mapping == 'true':
-    #     query = query.filter(vehicle_model.yahoo_id==None)
+            if not _order_by:
+                continue
 
-    # if order_by:
-    #     order_by_list = order_by.split(',')
-    #     for _order_by in order_by_list:
-    #         asc = True
-
-    #         if not _order_by:
-    #             continue
-
-    #         if _order_by[0] == '-' :
+            if _order_by[0] == '-' :
                 
-    #             if len(_order_by)<=1:
-    #                 continue
-    #             asc = False
-    #             _order_by = _order_by[1:]
+                if len(_order_by)<=1:
+                    continue
+                asc = False
+                _order_by = _order_by[1:]
 
-    #         if asc:
-    #             query = query.order_by(text(f'{_order_by} ASC'))
-    #         else:
-    #             query = query.order_by(text(f'{_order_by} DESC'))
+            if asc:
+                vehicles = vehicles.order_by(text(f'vehicle_model_{_order_by} ASC'))
+                # query = query.order_by(text(f'vehicle_model_{_order_by} ASC'))
+            else:
+                vehicles = vehicles.order_by(text(f'vehicle_model_{_order_by} DESC'))
+                # query = query.order_by(text(f'vehicle_model_{_order_by} DESC'))
 
     return paginate(vehicles)
     return paginate(_db, query)
